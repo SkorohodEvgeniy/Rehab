fed3424695f10877cc94c211ab4c9ac93c9ac523
Rock-n-Roll <himera80@ukr.net>
2023-01-22 15:08:19 +0200

    End footer

--- js/script.js
+++ js/script.js
@@ -18,3 +18,15 @@ clickBtn.addEventListener('click', function(){
   let headerMenu = document.querySelector('.header__btn-menu').classList.toggle('open');
 
 });
+
+
+/* Accordion*/
+$(document).ready(function () {
+  $(".accordion__title").click(function (event) {
+    if ($(".accordion").hasClass("one")) {
+      $(".accordion__title").not($(this)).removeClass("active");
+      $(".accordion__text").not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
