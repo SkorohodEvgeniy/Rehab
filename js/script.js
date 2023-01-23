d414573e14f3f6b148918022fa5c72b2944097ac
Rock-n-Roll <himera80@ukr.net>
2023-01-23 20:27:55 +0200

    Noot

--- js/script.js
+++ js/script.js
@@ -1,3 +1,38 @@
+let headerBurger = document.querySelector(".header__burger");
+let headerMenu = document.querySelector(".header__menu");
+let body = document.getElementsByTagName("body");
+
+function toggleBurgerMenu() {
+  headerBurger, headerMenu.classList.toggle("active");
+  body.classList.toggle("lock");
+}
+
+headerBurger.addEventListener("click", toggleBurgerMenu);
+
+/** Btn menu */
+
+let clickBtn = document.querySelector('.header__btn-link');
+
+clickBtn.addEventListener('click', function(){
+
+  let headerMenu = document.querySelector('.header__btn-menu').classList.toggle('open');
+
+});
+
+
+/* Accordion*/
+$(document).ready(function () {
+  $(".accordion__title").click(function (event) {
+    if ($(".accordion").hasClass("one")) {
+      $(".accordion__title").not($(this)).removeClass("active");
+      $(".accordion__text").not($(this).next()).slideUp(300);
+    }
+    $(this).toggleClass("active").next().slideToggle(300);
+  });
+});
+
+/**------------Slider reviews------------------------- */
+
 $(document).ready(function () {
   $(".carousel__box-lists").slick({
     dots: true,
