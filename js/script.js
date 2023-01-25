4658befb1511c893005c95dd423f678c7b041595
Rock-n-Roll <himera80@ukr.net>
2023-01-25 11:36:17 +0200

    Add Accordion to the page FAQ

--- js/script.js
+++ js/script.js
@@ -57,6 +57,39 @@ $(document).ready(function () {
   });
 });
 
+   // accordion 2ver.
+   (function () {
+    var initAccordion = function (itemsClass, headerClass, bodyClass) {
+        var $items = $(itemsClass);
+
+        $items.find(headerClass).on('click', function () {
+            $item = $(this).parent();
+            $items.not($item).removeClass('is-open');
+            $items.not($item).find(headerClass).removeClass('is-open');
+            $items.not($item).find(bodyClass).slideUp(250);
+
+
+            if ($(this).hasClass('is-open')) {
+                $item.removeClass('is-open');
+                $(this).removeClass('is-open');
+                $(this).siblings(bodyClass).slideUp(250);
+
+            } else {
+                $item.addClass('is-open');
+                $(this).addClass('is-open');
+                $(this).siblings(bodyClass).slideDown(250);
+
+            }
+        });
+    };
+
+    // suppliers
+    initAccordion('.faq__inner', '.faq__inner-header', '.faq__inner-body');
+
+
+})($);
+
+
 /**--------------Open POP UP------------------------------- */
 let openPopup = document.querySelector(".link-start");
 let application = document.querySelector(".application");
